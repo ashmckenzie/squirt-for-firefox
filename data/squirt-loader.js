ea969c3965154ddc9126b813c473d9d47692e974
Ash McKenzie <ash@the-rebellion.net>
2014-06-18 12:09:40 +1000

    initial commit

---
+++ data/squirt-loader.js
@@ -0,0 +1,8 @@
+sq = unsafeWindow.sq = unsafeWindow.sq || {};
+
+if (sq.loaded) {
+  sq.again();
+} else {
+  sq.bookmarkletVersion = '0.3.0';
+  sq.iframeQueryParams = { host: '//squirt.io', userId: '3789f85c-8e6d-4de0-b1be-8a05e68492a9'};
+}
